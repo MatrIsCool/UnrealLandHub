a23e790124e2b12a2ee02072a61d371af4e2a9d8
Elmoshlelmo <elmoshlelmo@gmail.com>
2015-04-06 01:35:40 +0100

    Added chatfilter

--- Hub/src/me/elmoshlelmo/hub/Hub.java
+++ Hub/src/me/elmoshlelmo/hub/Hub.java
@@ -1,9 +1,14 @@
 package me.elmoshlelmo.hub;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
 /**
  * Created by elmoshlelmo on 05/04/2015.
  * This code is free to use by must not be copied.
  * Thanks.
  */
-public class Hub {
+public class Hub extends JavaPlugin {
+
+
+
 }
---
+++ Hub/src/me/elmoshlelmo/hub/chatfilter/ChatEvent.java
@@ -0,0 +1,11 @@
+package me.elmoshlelmo.hub.chatfilter;
+
+import org.bukkit.event.Listener;
+
+/**
+ * Created by elmoshlelmo on 06/04/2015.
+ * This code is free to use by must not be copied.
+ * Thanks.
+ */
+public class ChatEvent implements Listener {
+}
